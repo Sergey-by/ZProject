9cad61b7efb7d48f1760b0468d8072efc67ce790
sergey-by <sergeyla@mail.ru>
2014-06-03 16:56:23 +0300

    output changed

--- models/gallery.js
+++ models/gallery.js
@@ -3,7 +3,7 @@ var resp = require('../libs/responses.js');
 module.exports = (function() {
   return {
   	showGallery: function(req, res) {
-		resp.respond(res, 200, 'ZProject in action!!! your request is :' + req.params.any_text);
+		resp.respond(res, 200, 'ZProject in action!!! your request is: ' + req.params.any_text);
     }
   }
 })();
